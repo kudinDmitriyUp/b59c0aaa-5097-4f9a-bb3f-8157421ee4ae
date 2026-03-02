d98d1be6c1b5548840d4741d77d81ab98b6c599f
bender <kudin.dima@gmail.com>
2026-03-02 09:57:10 +0000

    Update src/app/coffees/page.tsx

--- src/app/coffees/page.tsx
+++ src/app/coffees/page.tsx
@@ -47,23 +47,23 @@ export default function CoffeesPage() {
           products={[
             {
               id: "1",              brand: "Artisan Coffee Co.",              name: "Espresso",              price: "$4.99",              rating: 5,
-              reviewCount: "189",              imageSrc: "http://img.b2bpic.net/free-photo/young-man-working-with-coffee-machine-bar-counter-restaurant-barista-apron-white-shirt-making-coffee-by-coffee-machine-cafe-boy-working-as-barista-coffee-shop_574295-4942.jpg",              imageAlt: "Perfect espresso shot with crema"
+              reviewCount: "189",              imageSrc: "http://img.b2bpic.net/free-photo/young-man-working-with-coffee-machine-bar-counter-restaurant-barista-apron-white-shirt-making-coffee-by-coffee-machine-cafe-boy-working-as-barista-coffee-shop_574295-4942.jpg?_wi=2",              imageAlt: "Perfect espresso shot with crema"
             },
             {
               id: "2",              brand: "Artisan Coffee Co.",              name: "Cappuccino",              price: "$5.99",              rating: 5,
-              reviewCount: "276",              imageSrc: "http://img.b2bpic.net/free-photo/close-up-cup-coffee-latte-art-woman-hand-coffee-shop-cafe_1150-10238.jpg",              imageAlt: "Cappuccino with latte art"
+              reviewCount: "276",              imageSrc: "http://img.b2bpic.net/free-photo/close-up-cup-coffee-latte-art-woman-hand-coffee-shop-cafe_1150-10238.jpg?_wi=2",              imageAlt: "Cappuccino with latte art"
             },
             {
               id: "3",              brand: "Artisan Coffee Co.",              name: "Americano",              price: "$4.49",              rating: 5,
-              reviewCount: "142",              imageSrc: "http://img.b2bpic.net/free-photo/aerial-view-person-making-drip-coffee_53876-30607.jpg",              imageAlt: "Americano coffee"
+              reviewCount: "142",              imageSrc: "http://img.b2bpic.net/free-photo/aerial-view-person-making-drip-coffee_53876-30607.jpg?_wi=2",              imageAlt: "Americano coffee"
             },
             {
               id: "4",              brand: "Artisan Coffee Co.",              name: "Matcha Latte",              price: "$6.99",              rating: 5,
-              reviewCount: "203",              imageSrc: "http://img.b2bpic.net/free-photo/close-up-cup-coffee-latte-art-woman-hand-coffee-shop-cafe_1150-10238.jpg",              imageAlt: "Matcha latte with creamy foam"
+              reviewCount: "203",              imageSrc: "http://img.b2bpic.net/free-photo/close-up-cup-coffee-latte-art-woman-hand-coffee-shop-cafe_1150-10238.jpg?_wi=3",              imageAlt: "Matcha latte with creamy foam"
             },
             {
               id: "5",              brand: "Artisan Coffee Co.",              name: "Chaga Tea",              price: "$5.49",              rating: 5,
-              reviewCount: "98",              imageSrc: "http://img.b2bpic.net/free-photo/aerial-view-person-making-drip-coffee_53876-30607.jpg",              imageAlt: "Chaga tea blend"
+              reviewCount: "98",              imageSrc: "http://img.b2bpic.net/free-photo/aerial-view-person-making-drip-coffee_53876-30607.jpg?_wi=3",              imageAlt: "Chaga tea blend"
             }
           ]}
           gridVariant="uniform-all-items-equal"
@@ -84,7 +84,7 @@ export default function CoffeesPage() {
           ]}
           textarea={{ name: "message", placeholder: "Tell us about your coffee preferences...", rows: 5, required: true }}
           useInvertedBackground={false}
-          imageSrc="http://img.b2bpic.net/free-photo/rustic-chandelier-made-bulbs-ropes-dining-table-vintage-kitchen_181624-9173.jpg"
+          imageSrc="http://img.b2bpic.net/free-photo/rustic-chandelier-made-bulbs-ropes-dining-table-vintage-kitchen_181624-9173.jpg?_wi=2"
           imageAlt="Coffee shop interior"
           mediaAnimation="slide-up"
           buttonText="Send Message"
