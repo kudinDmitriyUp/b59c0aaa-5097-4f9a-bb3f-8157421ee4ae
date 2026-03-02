61556f6a7bdaa9d0f2a8aa1b5f41719e528d4183
bender <kudin.dima@gmail.com>
2026-03-02 10:19:56 +0000

    Update src/app/bagels/page.tsx

--- src/app/bagels/page.tsx
+++ src/app/bagels/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import NavbarStyleCentered from '@/components/navbar/NavbarStyleCentered/NavbarStyleCentered';
 import ProductCardTwo from '@/components/sections/product/ProductCardTwo';
